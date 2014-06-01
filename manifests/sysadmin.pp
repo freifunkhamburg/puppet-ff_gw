13d37b6a5447e84e09fe0329a5fd5ab9cf50f73f
Martin Schuette <info@mschuette.name>
2014-06-01 21:44:16 +0200

    disable atop daemon

--- manifests/sysadmin.pp
+++ manifests/sysadmin.pp
@@ -42,6 +42,12 @@ class ff_gw::sysadmin($zabbixserver = '127.0.0.1', $muninserver = '127.0.0.1', $
     'screen', 'tcpdump', 'rsync', 'file', 'psmisc']:
       ensure => installed,
   }
+  # stop atop daemon (cf. https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=506191)
+  service { 'atop':
+    ensure  => stopped,
+    enable  => false,
+    require => Package['atop']
+  }
 
   # user accounts
   create_resources('account', $accounts)
